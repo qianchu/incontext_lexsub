7107e829abecadd6f275de82ba945a25a8fb01fb
qianchu <hey_flora@126.com>
2018-08-27 22:16:46 +0000

    environmen setup

--- start.sh
+++ start.sh
@@ -1,21 +1,35 @@
-#run inside the docker nvidia-docker run  --name context2vec-gpu -it -p 8888:8888 -v /home/ql261/simp2trad/:/home/simp2trad/ chainer/chainer:v4.0.0-python2-lqc /bin/bash 
+#run inside nvidia-docker run -it -p 8886:8886  --name incontext_lexsub -it -v /home/ql261/incontext_lexsub/:/home/incontext_lexsub allennlp/allennlp-lqc /bin/bash
 
 #install optional
-apt-get update
-apt-get upgrade
-apt-get install git
+# sudo apt-get update
+# sudo apt-get upgrade
+# sudo apt-get install git
 git config --global user.email "hey_flora@126.com"
 git config --global user.name "qianchu"
 
 # set up bash shell
 # pack python project
+cd /home/incontext_lexsub
+#sudo python setup.py install
+#sudo pip install -U scipy
+#sudo pip install -U scikit-learn
+#sudo pip install matplotlib
+#sudo pip install nltk
+#sudo pip install --upgrade gensim
+# run jupyter
+cd /home/
+sudo chmod -R 777 ./*
+cd /home/incontext_lexsub
+# sudo python -m pip install --upgrade pip
+# sudo python -m pip install jupyter
+# sudo python -m pip install pandas
 
 if [ ! -d '/home/ql261/.jupyter/' ]; then
     sudo mkdir /home/ql261/.jupyter/
 fi
-sudo cp /home/context_specialized/jupyter_notebook_config.py /home/ql261/.jupyter/
+sudo cp /home/incontext_lexsub/jupyter_notebook_config.py /home/ql261/.jupyter/
 
 for pid in $(ps -def | grep jupyter | awk '{print $2}'); do sudo kill -9 $pid; done
 
 export SHELL=/bin/bash
-jupyter notebook --ip '*'  --port=8886 --allow-root &
+jupyter notebook --ip '*'  --port=8000 --allow-root &
