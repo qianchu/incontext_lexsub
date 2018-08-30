7ac20ed3e3af789f10dff4db80acc8e6642f8121
qianchu <hey_flora@126.com>
2018-08-30 23:08:16 +0000

    word sim elmo

--- eval/explore_elmo.py
+++ eval/explore_elmo.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 from allennlp.modules.elmo import Elmo, batch_to_ids
@@ -10,10 +10,16 @@ import numpy as np
 from scipy.stats import spearmanr
 
 
+# In[2]:
+
+
 options_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_options.json"
 weight_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
 
 elmo = Elmo(options_file, weight_file, 1, dropout=0)
+
+
+# In[3]:
 
 
 # def context_independent_sim(x,y):
